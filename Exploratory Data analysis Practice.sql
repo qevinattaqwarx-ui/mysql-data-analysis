ef32c642da7514daf25b4832153907aa0fb2a22b
Qevin Attaqwa <qevinattaqwa.rx@gmail.com>
2026-06-22 00:36:40 +0700

    Add files via upload

---
+++ Exploratory Data analysis Practice.sql
@@ -0,0 +1,117 @@
+-- Exploratory Data analysis
+
+
+select *
+from layoffs_stagging2;
+
+
+select max(total_laid_off), max(percentage_laid_off)
+from layoffs_stagging2;
+
+
+
+select *
+from layoffs_stagging2
+where percentage_laid_off = 1
+order by total_laid_off desc;
+
+select *
+from layoffs_stagging2
+where percentage_laid_off = 1
+order by funds_raised_millions desc;
+
+select company, sum(total_laid_off)
+from layoffs_stagging2
+group by company
+order by 2 desc; 
+
+
+select min(`date`), max(`date`)
+from layoffs_stagging2;
+
+
+select industry, sum(total_laid_off)
+from layoffs_stagging2
+group by industry
+order by 2 desc; 
+
+
+select *
+from layoffs_stagging2;
+
+
+select country, sum(total_laid_off)
+from layoffs_stagging2
+group by country
+order by 2 desc; 
+
+
+select year(`date`), sum(total_laid_off)
+from layoffs_stagging2
+group by year(`date`)
+order by 2 desc; 
+
+
+select stage, sum(total_laid_off)
+from layoffs_stagging2
+group by stage
+order by 2 desc; 
+
+select company, sum(percentage_laid_off)
+from layoffs_stagging2
+group by company 
+order by 2 desc; 
+
+
+select substring(`date`,1,7) as `Month`, sum(total_laid_off)
+from layoffs_stagging2
+where substring(`date`,1,7) is not null
+group by `Month`
+order by 1 asc;
+
+## cte
+with Rolling_Total as
+(
+select substring(`date`,1,7) as `Month`, sum(total_laid_off) as total_off
+from layoffs_stagging2
+where substring(`date`,1,7) is not null
+group by `Month`
+order by 1 asc
+)
+select `Month`, total_off
+,sum(total_off) over (order by `Month`) as Rolling_Total
+from Rolling_Total;
+
+
+select company, sum(total_laid_off)
+from layoffs_stagging2
+group by company
+order by 2 desc; 
+
+select company, Year(`date`), sum(total_laid_off)
+from layoffs_stagging2
+group by company, Year(`date`)
+order by 3 asc;
+
+
+ with  company_Year (company, years, total_laid_off) as 
+(
+select company, Year(`date`), sum(total_laid_off)
+from layoffs_stagging2
+group by company, Year(`date`)
+), company_year_rank as
+
+(select *, dense_rank () 
+over(partition by years order by total_laid_off desc) as ranking
+from company_Year
+where years is not null)
+
+select *
+from company_year_rank
+where ranking <= 5 
+;
+
+
+
+
+
